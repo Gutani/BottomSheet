d699cd54557c8871252d9681effae23583a92185
Gustavo Antunes <gustavoamalheiros@gmail.com>
2022-02-13 20:55:12 -0300

    Initial Commit

---
+++ BottomSheet/BottomSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottomSheetApp.swift
+//  BottomSheet
+//
+//  Created by Gustavo Malheiros on 13/02/22.
+//
+
+import SwiftUI
+
+@main
+struct BottomSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
